ce61cc102d55cc3277c920d8c0bfd8e42a8a67b2
aanchalarora786 <43212831+aanchalarora786@users.noreply.github.com>
2018-09-25 12:28:57 -0400

    Add files via upload

---
+++ INFO8170_assignment/INFO8170_assignment/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INFO8170_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            /// calling variables of rectangle class
+            int rectlength = rectangle.Length;
+            int rectwidth = rectangle.Width;
+
+            Console.WriteLine("Enter the Length of Rectangle");
+            string length = string.Empty;
+            length = Console.ReadLine();
+            //parsing  to int
+            rectlength = int.Parse(length);
+            Console.WriteLine("Enter the Width of Rectangle");
+            string width = string.Empty;
+            width = Console.ReadLine();
+            // /// parsing to int
+            rectwidth = int.Parse(width);
+            string userinput = string.Empty;
+            bool validMenuSelection = false;
+            while (validMenuSelection == false)
+            {
+
+
+
+                Console.WriteLine("1 Get Rectangle Length");
+                Console.WriteLine("2 Change Rectangle Length");
+                Console.WriteLine("3 to Get Rectangle Width");
+                Console.WriteLine("4 to Change Rectangle Width");
+                Console.WriteLine("5 to Get Rectangle Parameter");
+                Console.WriteLine("6 to Get Rectangle Area");
+                Console.WriteLine("7 to Exit");
+
+                userinput = Console.ReadLine();
+
+                switch (userinput)
+                {
+                    case "1":
+                        Console.WriteLine("Length of the Rectangle is {0} ", rectangle.SetLength(rectlength));
+
+                        break;
+                    case "2":
+                        Console.WriteLine("Enter new Length");
+                        length = Console.ReadLine();
+                        rectlength = int.Parse(length);
+                        Console.WriteLine("new length is {0} ", rectangle.SetLength(rectlength));
+                        break;
+                    case "3":
+                        Console.WriteLine("width is {0} ", rectangle.GetWidth());
+                        break;
+                    case "4":
+                        Console.WriteLine("Enter new width");
+                        width = Console.ReadLine();
+                        rectwidth = int.Parse(width);
+                        Console.WriteLine("new width is {0} ", rectangle.SetWidth(rectwidth));
+                        break;
+                    case "5":
+                        Console.WriteLine("perimeter is {0} ", rectangle.GetPerimeter());
+                        break;
+                    case "6":
+                        Console.WriteLine("Area is {0} ", rectangle.GetArea());
+                        break;
+                    case "7":
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("please enter correctly");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ INFO8170_assignment/TestCases/rectangle_TestCases.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INFO8170_assignment;
+using NUnit.Framework;
+
+namespace TestCases
+{
+    [TestFixture]
+    class rectangle_TestCases
+    {
+        [Test]
+        public void GetLength_input5_expectLengthequals5()
+        {   
+            //Arrange
+            int l = 4;
+            int w = 3;
+            Rectangle rect = new Rectangle(l,w);
+
+            //Act
+            int length = rect.GetLength();
+            //assert
+            Assert.AreEqual(length, l);
+        }
+        [Test]
+        public void GetWidth_input5_expectWidthequals5()
+        {
+            //Arrange
+            int l = 4;
+            int w = 3;
+            Rectangle rect = new Rectangle(l, w);
+
+            //Act
+            int width = rect.GetWidth();
+            //assert
+            Assert.AreEqual(width, w);
+            
+        }
+        [Test]
+        public void SetLength_input5_expectLengthequals5()
+        {
+            //Arrange
+            int length = 5;
+            Rectangle rect = new Rectangle();
+
+            //Act
+            rect.SetLength(length);
+
+            //Assert
+            Assert.AreEqual(length, 5);
+
+        }
+        [Test]
+        public void SetWidth_input5_expectLengthequals5()
+        {
+            //Arrange
+            int width = 5;
+            Rectangle rect = new Rectangle();
+
+            //Act
+            rect.SetWidth(width);
+
+            //Assert
+            Assert.AreEqual(width, 5);
+
+        }
+        [Test]
+        public void GetPerimeter_input2_expectLengthequals8()
+        {
+            //arrange
+            int l = 2;
+            int w = 2;
+            Rectangle rect = new Rectangle(l,w);
+
+
+            //act
+            int result = rect.GetPerimeter();
+            //assert
+            Assert.AreEqual(result, 8);
+
+
+
+        }
+        [Test]
+        public void GetArea_input2_expectLengthequals4()
+        {
+            //arrange
+            int l = 2;
+            int w = 2;
+            Rectangle rect = new Rectangle(l,w);
+
+            //act
+            var result = rect.GetArea();
+
+            //assert
+            Assert.AreEqual(result, 4);
+
+        }
+
+
+    }
+}
